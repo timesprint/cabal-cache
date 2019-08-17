9498d11dfe01e96fa4651ce7d3f6436fde9a0701
John Ky <newhoggy@gmail.com>
2019-08-17 08:14:07 +0300

    Polysemy version of version command

--- src/App/Commands.hs
+++ src/App/Commands.hs
@@ -1,10 +1,13 @@
 module App.Commands where
 
+import App.Commands.PolyVersion
 import App.Commands.SyncFromArchive
 import App.Commands.SyncToArchive
 import App.Commands.Version
 import Data.Semigroup               ((<>))
 import Options.Applicative
+
+{-# ANN module ("HLint: ignore Monoid law, left identity"  :: String) #-}
 
 commands :: Parser (IO ())
 commands = commandsGeneral
@@ -15,3 +18,4 @@ commandsGeneral = subparser $ mempty
   <>  cmdSyncFromArchive
   <>  cmdSyncToArchive
   <>  cmdVersion
+  <>  cmdPolyVersion
