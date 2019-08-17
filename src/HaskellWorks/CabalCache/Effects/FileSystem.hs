9184bfc331aeb8816739c87c8e5607e73a6f120c
John Ky <newhoggy@gmail.com>
2019-08-17 11:51:51 +0300

    FileSystem effect

---
+++ src/HaskellWorks/CabalCache/Effects/FileSystem.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE TemplateHaskell     #-}
+
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module HaskellWorks.CabalCache.Effects.FileSystem
+  ( FileSystem(..)
+  , runFileSystem
+  , readFile
+  , writeFile
+  ) where
+
+import Polysemy
+import Prelude  hiding (readFile, writeFile)
+
+import qualified Data.ByteString.Lazy as LBS
+
+data FileSystem m a where
+  ReadFile   :: FilePath -> FileSystem m LBS.ByteString
+  WriteFile  :: FilePath -> LBS.ByteString -> FileSystem m ()
+
+makeSem ''FileSystem
+
+runFileSystem :: Member (Embed IO) r
+  => Sem (FileSystem ': r) a
+  -> Sem r a
+runFileSystem = interpret $ \case
+  ReadFile fp -> embed $ LBS.readFile fp
+  WriteFile fp contents -> embed $ LBS.writeFile fp contents
