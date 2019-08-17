2329cbfc7ecfd92c5aec2d94e63b2588ea18ee67
John Ky <newhoggy@gmail.com>
2019-08-17 11:36:46 +0300

    Polysemy version of version

--- src/App/Commands/PolyVersion.hs
+++ src/App/Commands/PolyVersion.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes     #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -9,25 +10,32 @@ module App.Commands.PolyVersion
 
 import App.Commands.Options.Parser (optsVersion)
 import Data.List
+import Control.Lens ((&))
 import Data.Semigroup              ((<>))
 import Options.Applicative         hiding (columns)
+import Polysemy
 
 import qualified App.Commands.Options.Types         as Z
 import qualified Data.Text                          as T
 import qualified Data.Version                       as V
-import qualified HaskellWorks.CabalCache.IO.Console as CIO
 import qualified Paths_cabal_cache                  as P
+import qualified HaskellWorks.CabalCache.Effects.Console as E
 
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
 
 runPolyVersion :: Z.VersionOptions -> IO ()
-runPolyVersion _ = do
+runPolyVersion opts = runInEffect opts
+  & E.runConsole
+  & runM
+
+runInEffect :: Members '[E.Console] r
+  => Z.VersionOptions -> Sem r ()
+runInEffect _ = do
   let V.Version {..} = P.version
-
   let version = intercalate "." $ fmap show versionBranch
+  E.putStr $ "cabal-cache " <> T.pack version
 
-  CIO.putStrLn $ "cabal-cache " <> T.pack version
 
 cmdPolyVersion :: Mod CommandFields (IO ())
 cmdPolyVersion = command "poly-version"  $ flip info idm $ runPolyVersion <$> optsVersion
