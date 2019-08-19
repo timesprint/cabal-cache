d3ec9f18b491d70c4412686b4ae8b05aa2f148d5
John Ky <newhoggy@gmail.com>
2019-08-19 18:54:02 +0200

    New ghcpkg functions

--- src/HaskellWorks/CabalCache/Effects/GhcPkg.hs
+++ src/HaskellWorks/CabalCache/Effects/GhcPkg.hs
@@ -7,27 +7,62 @@
 {-# LANGUAGE TypeOperators       #-}
 
 module HaskellWorks.CabalCache.Effects.GhcPkg
-  ( runGhcPkg
+  ( testAvailability
+  , recache
+  , init
   ) where
 
 import Polysemy
 import Polysemy.Fail
-import Prelude       hiding (putStr, putStrLn)
+import Prelude       hiding (init)
 
-import qualified HaskellWorks.CabalCache.Effects.Console as E
 import qualified HaskellWorks.CabalCache.Effects.Process as E
 import qualified System.Exit                             as IO
 
 runGhcPkg :: Members
     [ E.Process
-    , E.Console
     , Fail
     ] r
   => [String]
-  -> Sem r ()
+  -> Sem r (Either IO.ExitCode ())
 runGhcPkg args = do
   hGhcPkg <- E.spawnProcess "ghc-pkg" args
   exitCode <- E.waitForProcess hGhcPkg
   case exitCode of
-    IO.ExitFailure _ -> fail "ERROR: Unable to recache package db"
-    _                -> return ()
+    IO.ExitFailure _ -> return (Left exitCode)
+    _                -> return (Right ())
+
+testAvailability :: Members
+  [ E.Process
+  , Fail
+  ] r
+  => Sem r ()
+testAvailability = do
+  result <- runGhcPkg ["--version"]
+  case result of
+    Left _   -> fail "ghcpkg command line tool unavailable"
+    Right () -> return ()
+
+recache :: Members
+  [ E.Process
+  , Fail
+  ] r
+  => FilePath
+  -> Sem r ()
+recache packageDb = do
+  result <- runGhcPkg ["recache", "--package-db", packageDb]
+  case result of
+    Left _   -> fail "pkgdb recache failed"
+    Right () -> return ()
+
+init :: Members
+  [ E.Process
+  , Fail
+  ] r
+  => FilePath
+  -> Sem r ()
+init packageDb = do
+  result <- runGhcPkg ["init", packageDb]
+  case result of
+    Left _   -> fail "pkgdb inititialisation failed"
+    Right () -> return ()
