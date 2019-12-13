840359ecfff57dd09c24b749e135d3100d5a6670
John Ky <newhoggy@gmail.com>
2019-12-13 11:05:40 +1100

    Stuff

---
+++ src/HaskellWorks/CabalCache/Effects/Core.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE UnicodeSyntax       #-}
+
+module HaskellWorks.CabalCache.Effects.Core
+  (-- mkCompilerContext
+  ) where
+
+import Control.Monad.Except
+import Data.Text            (Text)
+import Polysemy
+import Polysemy.Error
+import Polysemy.Fail
+
+import qualified HaskellWorks.CabalCache.Core  as C
+import qualified HaskellWorks.CabalCache.Types as Z
+import qualified Polysemy.Error                as E
+
+mkCompilerContext :: Members [Embed IO, Error Text] r
+  => Z.PlanJson
+  -> Sem r Z.CompilerContext
+mkCompilerContext planJson = do
+  result <- embed $ (runExceptT (C.mkCompilerContext planJson) :: IO (Either Text Z.CompilerContext))
+  case result of
+    Right cc -> return cc
+    Left e   -> throw e
